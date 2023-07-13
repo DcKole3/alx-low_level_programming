3c488bef5e0afcd5f9a0086b40f5ea5309a1c34c
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 11:09:10 -0700

    checker commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * Return: 0 = Success, otherwise EOF = error
 */
 int main(void)
-{  
+{
 const char *_s="\"Programming is like building a multilingual puzzle";
-puts(_s); 
+puts(_s);
 return (0);
 }

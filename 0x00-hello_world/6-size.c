40b821e1896725478de73de64df3c34d91b19030
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 10:14:41 -0700

    checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
  * main  - code entry point
  *return: Always 0 = Success, otherwise EOF = error
 */
-int main(void)
+int main(void)/*main allows for no input required*/
 {
 char a;
 int b;
 long int c;
 long long int d;
 float e;
-printf("Size of a %s: %lu byte(s)\n", "char", (unsigned long)sizeof(a));
-printf("Size of an %s: %lu byte(s)\n", "int", (unsigned long)sizeof(b));
-printf("Size of a %s: %lu byte(s)\n", "long int", (unsigned long)sizeof(c));
-printf("Size of a %s: %lu byte(s)\n", "long long int", (unsigned long)sizeof(d));
-printf("Size of a %s: %lu byte(s)\n", "float", (unsigned long)sizeof(e));
-       return (0); /*Allows for result to stdout as mainn type is int*/
+ printf("Size of a %s: %lu byte(s)\n", "char", (unsigned long)sizeof(a));
+ printf("Size of an %s: %lu byte(s)\n", "int", (unsigned long)sizeof(b));
+ printf("Size of a %s: %lu byte(s)\n", "long int", (unsigned long)sizeof(c));
+ printf("Size of a %s: %lu byte(s)\n", "long long int", (unsigned long)sizeof(d));
+ printf("Size of a %s: %lu byte(s)\n", "float", (unsigned long)sizeof(e));
+ return (0); /*Allows for result to stdout as mainn type is int*/
 }

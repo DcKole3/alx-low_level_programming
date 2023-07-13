5e6dd9d36abd5febf56e209a21300b29813f4055
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 11:11:04 -0700

    checker commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-const char *_s= "\"Programming is like building a multilingual puzzle";
+const char *_s = "\"Programming is like building a multilingual puzzle";
 puts(_s);
 return (0);
 }

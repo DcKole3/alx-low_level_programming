1a33a75f4b9520fad69d4691dd5e126446fa42e1
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 11:07:48 -0700

    checker commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-
-0;276;0c#include <stdio.h>
+#include <stdio.h>
 /**
  * main  - code entry point
  * Return: 0 = Success, otherwise EOF = error
 */
 int main(void)
 {  
-  const char *_s="\"Programming is like building a multilingual puzzle"; /*Declares s as a variable*/
-  puts(_s);  /*writes the string s and a trailing newline*/
-  return (0);
+const char *_s="\"Programming is like building a multilingual puzzle";
+puts(_s); 
+return (0);
 }

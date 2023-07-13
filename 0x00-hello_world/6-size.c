278522199bee9a658348095ff541914f692790dc
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 10:23:39 -0700

    checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main  - code entry point
- *Description: prints the size of various types on the computer it is compiled and run on
+ *Description: 'prints the size of various types on the computer it is compiled and run on'
  *return: Always 0 = Success, otherwise EOF = error
 */
-int main(void)
+int main(void)/*main requires no input*/
 {
 char a;
 int b;
@@ -14,7 +14,7 @@ float e;
 printf("Size of a %s: %lu byte(s)\n", "char", (unsigned long)sizeof(a));
 printf("Size of an %s: %lu byte(s)\n", "int", (unsigned long)sizeof(b));
 printf("Size of a %s: %lu byte(s)\n", "long int", (unsigned long)sizeof(c));
-printf("Size of a %s:%lu byte(s)\n", "long long int", (unsigned long)sizeof(d));
+printf("Size of a %s: %lu byte(s)\n", "long long int", (unsigned long)sizeof(d));
 printf("Size of a %s: %lu byte(s)\n", "float", (unsigned long)sizeof(e));
-return (0); /*Allows for result to stdout as mainn type is int*/
+return (0); /*Allows for result to stdout as main type is int*/
 }

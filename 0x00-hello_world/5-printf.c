d1e69cd78d4709d1217f9cfc682a488bb978eb8a
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 10:47:01 -0700

    checker

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main  - code entry point
  *return: Always 0 = Success, otherwise error
 */
-
+/*Desciption: prints out string to stdout*/
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n"); /*Declaring the string for standard output*/

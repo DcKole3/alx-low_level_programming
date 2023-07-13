5f4460ecafed104c527a9dae4739c4c89f31d378
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 09:47:17 -0700

    checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@ int b;
 long int c;
 long long int d;
 float e;
-printf("Size of a %s: %lu byte(s)\n", "char", (unsigned long)sizeof(a)); /**/
+printf("Size of a %s: %lu byte(s)\n", "char", (unsigned long)sizeof(a)); /*Displays result to stdout*/
 printf("Size of an %s: %lu byte(s)\n", "int", (unsigned long)sizeof(b));
 printf("Size of a %s: %lu byte(s)\n", "long int", (unsigned long)sizeof(c));
 printf("Size of a %s: %lu byte(s)\n", "long long int", (unsigned long)sizeof(d));
 printf("Size of a %s: %lu byte(s)\n", "float", (unsigned long)sizeof(e));
-       return (0);
+       return (0); /*Allows for result to stdout as mainn type is int*/
 }

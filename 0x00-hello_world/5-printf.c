fc1486f1ea092e6ec12eb923b7ef350f539f36f3
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 10:56:04 -0700

    checker commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main  - code entry point
  *return: Always 0 = Success, otherwise error
 */
-/*Desciption: prints out string to stdout*/
+/**Desciption: prints out string to stdout*/
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n"); /*Declaring the string for standard output*/
-    return (0);
+ Return:0;
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ printf("Size of an %s: %lu byte(s)\n", "int", (unsigned long)sizeof(b));
 printf("Size of a %s: %lu byte(s)\n", "long int", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a %s: %lu byte(s)\n", "float", (unsigned long)sizeof(e));
-return (0); /*Allows for result to stdout as main type is int*/
+ Return:0; /*Allows for result to stdout as main type is int*/
 }

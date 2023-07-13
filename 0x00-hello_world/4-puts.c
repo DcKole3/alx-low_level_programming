0e6ab4ca4583c9f7c232649f1e848654e2b4e830
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 08:18:25 -0700

    fourth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main  - code entry point
+ *return: Always 0 = Success, otherwise EOF = error
+*/
+int main(void)
+{  
+  const char *_s="\"Programming is like building a multilingual puzzle"; /*Declares s as a variable*/
+  puts(_s);  /*writes the string s and a trailing newline*/
+  return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main  - code entry point
+ *return: Always 0 = Success, otherwise error
+*/
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n"); /*Declaring the string for standard output*/
+    return (0);
+}

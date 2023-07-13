a6f3e58a8124081dedc870711305b0f67f07aa83
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-13 09:19:11 -0700

    checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,19 +3,16 @@
  * main  - code entry point
  *return: Always 0 = Success, otherwise EOF = error
 */
-
 int main(void)
-{
-  char a;
-  int b;
-  long int c;
-  long long int d;
-  float e;
+{  char a;
+int b;
+long int c;
+long long int d;
+float e;
   printf("Size of a %s: %lu byte(s).\n","char",(unsigned long)sizeof(a)); /**/
-  printf("Size of a %s: %lu byte(s).\n","int",(unsigned long)sizeof(b));
+  printf("Size of an %s: %lu byte(s).\n","int",(unsigned long)sizeof(b));
   printf("Size of a %s: %lu byte(s).\n","long int",(unsigned long)sizeof(c));
   printf("Size of a %s: %lu byte(s).\n","long long int",(unsigned long)sizeof(d));
   printf("Size of a %s: %lu byte(s).\n","float",(unsigned long)sizeof(e));
-   
-    return (0);
+       return (0);
 }

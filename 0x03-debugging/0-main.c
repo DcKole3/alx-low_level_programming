b1913de4f192090364ac91d2dbb5096ce27fac24
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-18 14:03:30 -0700

    commit 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+11;rgb:0000/0000/0000
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}

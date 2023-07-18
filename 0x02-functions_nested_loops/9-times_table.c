f684775da10cfdf6fd3a7ecbb961cd8b9c654149
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-18 10:29:08 -0700

    commitment

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,19 +9,21 @@ int i = 0;
 int res;
 while (i <= 9)
 {
-for (j=0; j <= 9; j++)
+for (j = 0; j <= 9; j++)
 {
 res = i * j;
 if (j == 0)
 {
 _putchar('0');
-} else if (res <= 9)
+}
+else if (res <= 9)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(res + '0');
-} else
+}
+else
 {
 _putchar(',');
 _putchar(' ');
@@ -29,6 +31,7 @@ _putchar(res / 10 + '0');
 _putchar(res % 10 + '0');
 }
 }
-_putchar('\n');                                                                                                                       i++; 
+_putchar('\n');
+i++; 
 }
 }

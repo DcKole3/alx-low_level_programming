ea795f4300be8aaf8b35f4263a7fb1dcf0ab6f88
Kolawole Taiwo <dayokole3@gmail.com>
2023-07-31 07:14:57 -0700

    commit all

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,7 +7,8 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-  int i,j;
+int i;
+int j;
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,8 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i,j;
+int i;
+int j;
 if (needle[0] == '\0')
 return (haystack);
 for (i = 0; haystack[i] != '\0'; i++)

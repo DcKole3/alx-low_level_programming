f33c35cbd68781083e4c5d7b60b959544a880233
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-08 06:16:42 -0700

    commit all

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,22 +4,23 @@
 /**
  * _strdup - allocate memory = size of str
  * @str: parameter string to copy
+ * Return: pointer to new memory
  */
 char *_strdup(char *str)
 {
 int i;
 int j = 0;
 char *arr;
+if (str == NULL)
+{
+return (NULL);
+}
 while (str[j] != '\0')
 {
 j++;
 }
 arr = malloc(sizeof(char) * j + 1);
-if (str == NULL)
-{
-return (NULL);
-}
-if (arr ==NULL)
+if (arr == NULL)
 return (NULL);
 for (i = 0; i < j; i++)
 {

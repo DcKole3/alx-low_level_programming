e9b7cc66b17d88b0e9de7f7f17c3e8cca27c9cf2
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-16 09:00:44 -0700

    submit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,12 +9,12 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
+	      {"+", op_add},
+	      {"-", op_sub},
+	      {"*", op_mul},
+	      {"/", op_div},
+	      {"%", op_mod},
+	      {NULL, NULL}
 };
 int i;
 i = 0;
@@ -23,8 +23,8 @@ while (ops[i].f != NULL)
 if (*s == *(ops[i].op) && s[1] == '\0')
 {
 return (ops[i].f);
-i++;
 }
+i++;
 }
 printf("Error\n");
 exit(99);

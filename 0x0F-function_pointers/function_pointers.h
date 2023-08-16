0a01fd8fb22a079c836fd913a12ce1ccf7116ef8
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-16 04:45:29 -0700

    adding files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 /**
  * print_name
  * prints a name using call back func.
@@ -16,8 +16,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
  * searches for an integer in an array
  */
 int int_index(int *array, int size, int (*cmp)(int));
-/**
- * 
- * 
- */
 #endif /*FUNCTION_POINTERS_H*/

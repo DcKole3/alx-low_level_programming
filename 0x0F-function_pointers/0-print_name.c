4044e785644bac32f78a8bb6e91aff232c307050
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-16 04:39:42 -0700

    Error fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * print_name - prints name using callback func.
  * @name: name to print
@@ -6,5 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
 f(name); 
 }

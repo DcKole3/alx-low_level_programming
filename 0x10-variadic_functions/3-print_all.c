0da363a355e9d9adda8420a95852f86efaede76f
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-17 07:44:56 -0700

    commit all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,32 +3,87 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_all - prints anything
+ * print_c - prints char
+ * @pr: arguments list
+ * Return: 0 always
+ */
+int print_c(va_list pr)
+{
+printf("%c", va_arg(pr, int));
+return (0);
+}
+/**
+ * print_i - prints int
+ * @pr: arguments list
+ * Return: 0 always
+ */
+int print_i(va_list pr)
+{
+printf("%d", va_arg(pr, int));
+return (0);
+}
+/**
+ * print_f - prints float
+ * @pr: arguments list
+ * Return: 0 always
+ */
+int print_f(va_list pr)
+{
+printf("%f", va_arg(pr, double));
+return (0);
+}
+/**
+ * print_s - prints string
+ * @pr: arguments list
+ * Return: 0 always
+ */
+int print_s(va_list pr)
+{
+char *string;
+string = va_arg(pr, char *);
+if (string == NULL)
+{
+printf("(nil)");
+return (0);
+}
+printf("%s", string);
+return (0);
+}
+/**
+ * print_all - prints all
  * @format: list of types of arguments
  */
 void print_all(const char * const format, ...)
 {
-va_list pr;
-int i = 0;
-char *arr;
-arr = {'c', 'i', 'f', 's'};
-char *string; 
-va_start(pr, format);
- string = va_arg(pr, char *);
- while (arr == format)
-   {
-     printf(va_arg(pr, char));
-     if (i > 0 && sep != NULL)
+int i;
+int j;
+char *sep1 = "";
+char *sep2 = ", ";
+va_list printall;
+pr_all ops[] = {
+{"c", print_c},
+{"i", print_i},
+{"s", print_s},
+{"f", print_f},
+{NULL, NULL}
+};
+va_start(printall, format);
+i = 0;
+while (format != NULL && format[i])
 {
-  printf(", ");
+j = 0;
+while (ops[j].f != NULL)
+{
+if (format[i] == *(ops[j].c))
+{
+printf("%s", sep1);
+ops[j].f(printall);
 }
-
- if (string == NULL)
-   {
-     printf("(nil)");
-   }
- i++;
-   }
-va_end(pr);
+j++;
+}
+sep1 = sep2;
+i++;
+}
 printf("\n");
+va_end(printall);
 }

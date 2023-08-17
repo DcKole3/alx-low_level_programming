744d3a2d0471148552de14413a42e70170557a06
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-17 07:28:12 -0700

    commit all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+int print_c(va_list pr);
+int print_i(va_list pr);
+int print_f(va_list pr);
+int print_s(va_list pr);
+/**
+ * struct op - struct for array
+ * @c: character to compare to
+ * @f: corelating function
+ */
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list pr);
+} pr_all;
 #endif /*VARIADIC_FUNCTIONS_H*/

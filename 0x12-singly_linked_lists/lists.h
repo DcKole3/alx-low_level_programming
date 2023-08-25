1f16f50db9f3d15f4fbf61cc6ce3fbfbfd87f3d9
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-25 09:25:34 -0700

    commit all

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,25 +20,25 @@ typedef struct list_s
  * print_list -
  * prints elements of a list
  */
-size_t print_list(const list_t *h)
+size_t print_list(const list_t *h);
 /**
  * list_len -
  * gets no of elements in a linked list
  */
-size_t list_len(const list_t *h)
+size_t list_len(const list_t *h);
 /**
  * add_node  -
  * adds new node at list start
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str);
 /**
  * add_node_end  -
  * adds new node at list end
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str);
 /**
  * free_list -
  * frees a linked list
  */
-void free_list(list_t *head)
+void free_list(list_t *head);
 #endif

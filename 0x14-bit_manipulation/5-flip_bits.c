d4a0a58e326e191117b954e0bb120ac00b8aac8a
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-31 09:09:43 -0700

    correct and commit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,12 +2,13 @@
 #include "2-get_bit.c"
 /**
  * flip_bits - gets no of bits needed to flip
- * one number to another 
+ * one number to another
  * @n: int 1 to compare
  * @m: int 1 to compare
  * Return: number of flips
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n,
+		       unsigned long int m)
 {
 unsigned int i = 0;
 unsigned long int xor;

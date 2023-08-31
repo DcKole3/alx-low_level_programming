e953256000be4517b64659f20129fc6ff3b8612b
Kolawole Taiwo <dayokole3@gmail.com>
2023-08-31 09:14:50 -0700

    correct and commit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,19 @@
 #include "main.h"
 #include "2-get_bit.c"
 /**
- * set_bit -  sets value of bit to 0 at index
+ * clear_bit -  sets value of bit to 0 at index
  * @n: pointer to integer to apply funct
  * @index: position of bit to set
  * Return: 1, or -1 (Error)
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index > 32)
 return (-1);
 (*n) &= ~(1 << index);
 if (get_bit((*n), index) == 0)
+{
 return (1);
+}
 return (-1);
 }

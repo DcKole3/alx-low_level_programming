d040847ead117de94a0f37005dbc52b96597d6d8
Kolawole Taiwo <dayokole3@gmail.com>
2023-10-03 10:15:54 -0700

    task 1 & 2 commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * create_file - creates a file
- * @filename: filename to create
- * @text_content: string to write to file
- * Return: 1 on success, or -1 (fail)
+ * append_text_to_file - appends to end of file
+ * @filename: name of file
+ * @text_content: string to add at end of file
+ * Return: 1 on success and -1
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int dir, len = 0;
 long int wrote;
 if (filename == NULL)
 return (-1);
-dir = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+dir = open(filename, O_RDWR | O_APPEND);
 if (dir == -1)
 return (-1);
 if (text_content != NULL)

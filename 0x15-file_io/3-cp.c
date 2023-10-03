ec5dda04a3b102c1e8475a4acd0998624330e356
Kolawole Taiwo <dayokole3@gmail.com>
2023-10-03 10:29:39 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,31 +7,29 @@
  */
 int main(int ac, char **av)
 {
-int fdFrum, fdTo;
+int fdFrum, fdToo;
 int wrote, readed;
 char buff[1024];
 if (ac != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from 
-file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 fdFrum = open(av[1], O_RDONLY);
 if (fdFrum == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read 
-from file %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", av[1]);
 exit(98);
 }
-fdTo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fdTo == -1)
+fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fdToo == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 exit(99);
 }
 while ((readed = read(fdFrum, buff, 1024)) > 0)
 {
-wrote = write(fdTo, buff, readed);
+wrote = write(fdToo, buff, readed);
 if (wrote == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
@@ -40,7 +38,7 @@ exit(99);
 }
 if (readed == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", av[1]);
 exit(98);
 }
 if (close(fdFrum) == -1)
@@ -48,9 +46,9 @@ if (close(fdFrum) == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
 exit(100);
 }
-if (close(fdTo) == -1)
+if (close(fdToo) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdTo);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
 exit(100);
 }
 return (0);
